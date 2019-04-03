7761f409663f640271122fe0f0360a5220df1b20
CMonkey <42.codemonkey@gmail.com>
2019-04-03 14:36:25 +0800

    add KamonApp.start stop trance

--- src/main/scala/org/excavator/kamon/KamonApp.scala
+++ src/main/scala/org/excavator/kamon/KamonApp.scala
@@ -1,5 +1,17 @@
 package org.excavator.kamon
 
+import kamon.Kamon
+
 class KamonApp {
 
+  def start() = {
+    Kamon.start()
+  }
+
+  def stop() = {
+    Kamon.shutdown()
+  }
+
+  def trance() = {
+  }
 }
